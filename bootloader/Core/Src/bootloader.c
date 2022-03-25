e8dd7ace3d65dd273b6e9b3789ae8bd8146fdbd6
charles <chain40@outlook.com>
2022-03-25 22:21:45 +0800

    依 okgo 需求修改 bootloader
    1) 新增 crc16 驗證
    2) 新增 signature 判斷, signature = 0xF1517A66
    3) 修改DFU / APP 的位置以及大小配置

--- bootloader/Core/Src/bootloader.c
+++ bootloader/Core/Src/bootloader.c
@@ -8,39 +8,33 @@
 
 /* Flash & Sram layout
  * ---------------------------------------------------------------------------------------
- * | Bootloader    |  16KB | 0x08000000 ~ 0x08003FFF   | use Flash sector 0				|
- * | Reserved      |  16KB | 0x08004000 ~ 0x08007FFF   | use Flash sector 1				|  
- * | DFU		   | 244KB | 0x08008000 ~ 0x0803FFFF   | use Flash sector 2, 3, 4, 5    |
+ * | Bootloader    |  64KB | 0x08000000 ~ 0x0800FFFF   | use Flash sector 0, 1, 2, 3    |
+ * | DFU		   | 192KB | 0x08010000 ~ 0x0803FFFF   | use Flash sector 4, 5          |
  * | Applicartion  | 256KB | 0x08040000 ~ 0x0807FFFF   | use Flash sector 6, 7			| 
- * | BCB Magic     |   4 B | 0x2001FFFC ~ 0x2001FFFF   | use on-chip SRAM				|
+ * | BCB Magic     |   8 B | 0x2001FFF8 ~ 0x2001FFFF   | use on-chip SRAM				|
  * ---------------------------------------------------------------------------------------
  **/
 
-#define MY_SIGNATURE        0x08040123
-
-// DFU & APP 
-#define DFU_BASE		    0x08008000
-#define DFU_MAX_SIZE	    0x00038000
-
-#define DFU_SIGNATURE_BASE  (DFU_BASE + 0x20)
-#define DFU_SIGNATURE_SIZE  0x00000004
-#define DFU_SIGNATURE_VALUE MY_SIGNATURE
-
-#define APP_BASE		    0x08040000
-#define APP_MAX_SIZE	    0x00038000
-
-#define APP_BYTES_BASE      (APP_BASE + APP_MAX_SIZE)
-#define APP_BYTES_SIZE      0x00000004
-#define APP_CHKSUM_BASE     (APP_BYTES_BASE + APP_BYTES_SIZE) 
-#define APP_CHKSUM_SIZE	    0x00000004
-
+#define MY_SIGNATURE        0xF1517A66
 #define APP_SIGNATURE_BASE  (APP_BASE + 0x20)
 #define APP_SIGNATURE_SIZE  0x00000004
 #define APP_SIGNATURE_VALUE MY_SIGNATURE
 
+// DFU & APP 
+#define DFU_BASE		    0x08010000
+#define DFU_MAX_SIZE	    0x00030000
+
+#define APP_BASE		    0x08040000
+#define APP_MAX_SIZE	    0x00030000
+
 // Boot Ctrl Block (BCB)
-#define BCB_MAGIC_BASE	0x2001FFFC
+#define BCB_MAGIC_BASE	0x2001FFF8
 #define BCB_MAGIC_SIZE	0x00000004
+#define BCB_LEN_BASE	0x2001FFFC
+#define BCB_LEN_SIZE	0x00000002
+#define BCB_CRC16_BASE	0x2001FFFE
+#define BCB_CRC16_SIZE	0x00000002
+
 #define DFU_MAGIC_VALUE 0x12345678
 
 // Utility
@@ -57,147 +51,20 @@
 #define DFU_ABORD_REQ       0x00EE
 #define DFU_CPLT_REQ        0x00FF
 
-static bool prvDfuStartReq(void) {
-    #define DFU_START_RSP 0xCC33
-    uint16_t dfu_start_req = DFU_START_REQ;
-    uint16_t dfu_start_rsp = 0;
-    if (bSplSend(&dfu_start_req, sizeof(dfu_start_req)) == false) {
-        return false;
-    }
-    
-    uint16_t recv_size = 0;
-    if (bSplRecv(&dfu_start_rsp, &recv_size, sizeof(dfu_start_rsp)) == false) {
-        return false;
-    }   
-    
-    if (dfu_start_rsp != DFU_START_RSP) {
-        return false; 
-    }
-    
-    return true;
-}
 
 static uint32_t prvDfuSizeReq(void) {    
-    uint16_t dfu_size_req = DFU_SIZE_REQ;
-    if (bSplSend(&dfu_size_req, sizeof(dfu_size_req)) == false) {
-        return false;
-    }
-
-    uint32_t dfu_size = 0;
-    uint16_t recv_size = 0;
-    if (bSplRecv(&dfu_size, &recv_size, sizeof(dfu_size)) == false) {
-        return false;
-    }    
-    
-    if (recv_size != sizeof(dfu_size)) {
-        return false;
-    }
-    
+    uint16_t dfu_size = *(uint16_t *)BCB_LEN_BASE;
     return dfu_size;
 }
 
 static uint32_t prvDfuChkSumReq(void) {
-    uint16_t dfu_chksum_req = DFU_CHKSUM_REQ;
-    if (bSplSend(&dfu_chksum_req, sizeof(dfu_chksum_req)) == false) {
-        return false;
-    }
-
-    uint16_t dfu_chksum = 0;
-    uint16_t recv_size = 0;
-    if (bSplRecv(&dfu_chksum, &recv_size, sizeof(dfu_chksum)) == false) {
-        return false;
-    }    
-    
-    if (recv_size != sizeof(dfu_chksum)) {
-        return false;
-    }
-    
+    uint16_t dfu_chksum = *(uint16_t *)BCB_CRC16_BASE;
     return dfu_chksum;
 }
 
-static void prvDfuAbortReq(void) {
-    uint16_t dfu_abort_req = DFU_ABORD_REQ;
-    bSplSend(&dfu_abort_req, sizeof(dfu_abort_req));
-}
-
-static void prvDfuCpltReq(void) {
-    uint16_t dfu_cplt_req = DFU_CPLT_REQ;
-    bSplSend(&dfu_cplt_req, sizeof(dfu_cplt_req));
-}
-
-static bool prvDfuFlashErase(void) {
-    #define FLASH_TIMEOUT 5000
-    const uint32_t dfu_sectors[] = { 2, 3, 4, 5 };
-    bool ret = false;
-    HAL_FLASH_Unlock();
-    for (int i = 0; i < COUNTOF(dfu_sectors); i++) {
-        FLASH_Erase_Sector(dfu_sectors[i], FLASH_VOLTAGE_RANGE_3);
-        HAL_StatusTypeDef status = FLASH_WaitForLastOperation(FLASH_TIMEOUT);
-        if (status != HAL_OK) {
-            goto __EXIT;
-        }
-    }
-    ret = true;
-    
-__EXIT:
-    HAL_FLASH_Lock();    
-    return ret;
-}
-
-static uint32_t prvDfuSegDataReq(void *pvDst, uint32_t ulBase, uint32_t ulSize) {
-    struct __attribute__((packed)) {
-        uint16_t usReqID;
-        uint32_t ulOffset;
-        uint32_t ulSize;
-    } xSegDatReq = {
-        .usReqID = DFU_SEG_DATA_REQ,
-        .ulOffset = ulBase,
-        .ulSize = ulSize
-    };    
-    
-    if (bSplSend(&xSegDatReq, sizeof(xSegDatReq)) == false) {
-        return false;
-    }
-    
-    uint16_t recv_size = 0;
-    if (bSplRecv(pvDst, &recv_size, ulSize) == false) {
-        return false;
-    }        
-    
-    return recv_size;       
-}
-
-static uint32_t prvDfuSegChkSumReq(uint32_t ulBase, uint32_t ulSize) {
-    struct __attribute__((packed)) {
-        uint16_t usReqID;
-        uint32_t ulOffset;
-        uint32_t ulSize;
-    } xSegDatReq = {
-        .usReqID = DFU_SEG_CHKSUM_REQ,
-        .ulOffset = ulBase,
-        .ulSize = ulSize
-    };    
-    
-    if (bSplSend(&xSegDatReq, sizeof(xSegDatReq)) == false) {
-        return false;
-    }
-    
-    uint16_t dfu_seg_chksum = 0;
-    uint16_t recv_size = 0;
-    if (bSplRecv(&dfu_seg_chksum, &recv_size, sizeof(dfu_seg_chksum)) == false) {
-        return false;
-    }       
-    
-    return dfu_seg_chksum;   
-}
-
 static uint32_t prvDfuChkSumCal(void *pvDst, uint32_t ulSize) {
-    uint16_t chk_sum = 0;
-    uint8_t *p = pvDst;
-    for (uint32_t i = 0; i < ulSize; i++) {
-        chk_sum += p[i];
-    }
-    
+    extern unsigned int CRC16(unsigned char * pucFrame, unsigned int usLen);
+    uint16_t chk_sum = CRC16(pvDst, ulSize);
     return chk_sum;
 }
 
@@ -270,101 +137,35 @@ __EXIT:
     return ret;
 }
 
-static bool prvIsDfuSignatureInvalid(void) {
-    return *(uint32_t *)DFU_SIGNATURE_BASE != DFU_SIGNATURE_VALUE;
-}
-
 static void prvDfuMode(void) {
+	// get size of dfu image
     uint32_t dfu_size = prvDfuSizeReq();
     if (dfu_size < 448 || dfu_size > DFU_MAX_SIZE) {
-        goto __EXIT;
+        return;
     }        
+	// get checksum of dfu image
     uint32_t dfu_chksum = prvDfuChkSumReq();
     if (dfu_chksum == 0) {
-        goto __EXIT;
-    }     
-    if (prvDfuFlashErase() == false) {
-        goto __EXIT;
-    }    
-    uint32_t offset = 0;
-    uint32_t retry = 0;
-    do {
-        static  uint8_t data[1024];        
-        uint32_t req_size = MIN(dfu_size - offset, sizeof(data));        
-        uint32_t data_size = prvDfuSegDataReq(data, offset, req_size);
-        uint32_t chksum = prvDfuSegChkSumReq(offset, data_size);
-        if (prvDfuChkSumCal(data, data_size) != chksum) {
-            if (retry++ < 3) {
-                continue; 
-            } else { 
-                goto __EXIT;
-            }
-        }        
-        
-        void *pvDst = (void *)(DFU_BASE + offset);
-        if (prvFlashProgram(pvDst, data, data_size) == false) {
-            if (retry++ < 3) {
-                continue; 
-            } else {
-                goto __EXIT;
-            }
-        }    
-        
-        offset += data_size;
-        retry = 0;
-        
-    } while (offset < dfu_size);
-    
-    // check dfu image signature
-    if (prvIsDfuSignatureInvalid()) {
-        goto __EXIT;
-    } 
-    
-    // check whole dfu image
+        return;
+    }
+	// check whole dfu image
     if (prvDfuChkSumCal((void *)DFU_BASE, dfu_size) != dfu_chksum) {
-        goto __EXIT;
+        return;
     }
-    
-    // erase application
+	// erase application
     if (prvAppFlashErase() == false) {
-        goto __EXIT;
-    }    
-    
-    // copy dfu image to application
-    if (prvFlashProgram((void *)APP_BASE, (void *)DFU_BASE, dfu_size) == false) {
-        goto __EXIT;
+        return;
     }
-    
-    // compare dfu image vs application
+	// copy dfu image to application
+    if (prvFlashProgram((void *)APP_BASE, (void *)DFU_BASE, dfu_size) == false) {
+        return;
+    }
+	// compare dfu image vs application
     for (int i = 0; i < dfu_size; i++) {
         if (((uint8_t *)APP_BASE)[i] != ((uint8_t *)DFU_BASE)[i]) {
-            goto __EXIT;  
+			return;
         }
-    }   
-    
-    // program app size
-    if (prvFlashProgram((void *)APP_BYTES_BASE, (void *)&dfu_size, APP_BYTES_SIZE) == false) {
-        goto __EXIT;
-    }
-    if(*(uint32_t *)APP_BYTES_BASE != dfu_size){
-        goto __EXIT;
-    }
-    
-    // program app check sum
-    if (prvFlashProgram((void *)APP_CHKSUM_BASE, (void *)&dfu_chksum, APP_CHKSUM_SIZE) == false) {
-        goto __EXIT;
-    }
-    
-    if(*(uint32_t *)APP_CHKSUM_BASE != dfu_chksum){
-        goto __EXIT;
-    }
-    
-    prvDfuCpltReq();
-    
-    return;    
-    
-__EXIT:
-    prvDfuAbortReq();
+    }  
 }
 
 static void prvBootCtrlBlockReset(void) {
@@ -373,31 +174,17 @@ static void prvBootCtrlBlockReset(void) {
 
 static bool prvIsDfuMagicValid(void) {
     bool valid = *(uint32_t *)BCB_MAGIC_BASE == DFU_MAGIC_VALUE;
-    prvBootCtrlBlockReset();
     return valid;
 }
 
-static bool prvIsAppSignatureInvalid(void) {
-    return *(uint32_t *)APP_SIGNATURE_BASE != APP_SIGNATURE_VALUE;
-}
-
-static bool prvIsAppChkSumInvalid(void) {
-    return prvDfuChkSumCal((void *)APP_BASE, *(uint32_t *)APP_BYTES_BASE) != *(uint32_t *)APP_CHKSUM_BASE;
+static bool prvIsAppSignatureValid(void) {
+    return *(uint32_t *)APP_SIGNATURE_BASE == APP_SIGNATURE_VALUE;
 }
 
 static bool prvEnterDfuMode(void) {
-    if (prvIsDfuMagicValid()) {
+    if (prvIsDfuMagicValid() && prvIsAppSignatureValid()) {
         return true;
     }    
-    
-    if (prvIsAppSignatureInvalid()) {
-        return true;
-    }    
-    
-    if (prvIsAppChkSumInvalid()) {
-        return true;
-    }    
-    
     return false;
 }
 
@@ -413,12 +200,8 @@ static void prvApplicationJump(uint32_t ulVectorTabAddr) {
 void vBootloader(void) {
     // Enter Dfu Mode ?
     if (prvEnterDfuMode()) {        
-        for (int i = 0; i < 10; i++) {
-            if (prvDfuStartReq()) {
-                prvDfuMode();
-                break;
-            }
-        }
+        prvDfuMode();
+        prvBootCtrlBlockReset();
         HAL_NVIC_SystemReset();
     }       
     
